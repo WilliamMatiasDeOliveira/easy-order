5279a77ae203873aed8d5edffac78bd15185b0fa
William Matias de Oliveira <williammatiasjau@hotmail.com>
2025-05-04 16:01:09 -0300

    create header

--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<header class="container-fluid">
+    <div class="row col-12">
+
+            <div class="col-3 box-logo">
+                <img src="{{ asset('assets/imgs/logo.png') }}" alt="logomarca">
+                <span>Easy Order</span>
+            </div>
+
+            <div class="navbar col-6">
+                <ul>
+                    <li><a href="#">item 1</a></li>
+                    <li><a href="#">item 2</a></li>
+                    <li><a href="#">item 3</a></li>
+                    <li><a href="#">item 4</a></li>
+                    <li><a href="#">item 5</a></li>
+                </ul>
+            </div>
+
+            <div class="user-data col-3">
+                <div class="username">
+                    <i class="fa-solid fa-user"></i>
+                    <span>[username]</span>
+                </div>
+
+                <form action="#" method="post">
+                    <input type="submit"value="Logout"class="btn btn-danger">
+                </form>
+            </div>
+
+
+
+    </div>
+</header>
--- resources/views/Layouts/main_layout.blade.php
+++ resources/views/Layouts/main_layout.blade.php
@@ -10,6 +10,10 @@
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     {{-- link font awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    {{-- fonts google --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"rel="stylesheet">
     {{-- link reset.css --}}
     <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
     {{-- link style.css --}}
@@ -21,7 +25,9 @@
     @yield('content')
 
     {{-- link script.js --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
